aaa41561beb1115cc9c67b0deecca076f7e0d6f0
Dr. Peter Poeml <poeml@cmdline.net>
2006-05-10 14:21:51 +0000

    - reorganize source to build a python module - add setup.py - add osc-wrapper.py as a wrapper script, so the module can be used when   installed in the system as well as in the uninstalled source directory

---
+++ osc-wrapper.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+from osc import commandline
+from osc.core import init_basicauth
+
+init_basicauth()
+commandline.main()
+
---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='osc',
+      version='0.2',
+      description='opensuse commander',
+      author='Peter Poeml',
+      author_email='poeml@suse.de',
+      license='GPL',
+      url='https://forgesvn1.novell.com/svn/opensuse/trunk/buildservice/src/clientlib/python/osc/',
+
+      packages=['osc'],
+      scripts=['osc_hotshot.py', 'osc-wrapper.py'],
+
+     )
+
