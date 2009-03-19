6bf55c5415eab8c8e62d1c23106764bd007c75a9
Marcus Hüwe <suse-tux@gmx.de>
2009-03-19 13:58:04 +0000

    - forgot the usual license stuff...

--- osc/util/cpio.py
+++ osc/util/cpio.py
@@ -1,7 +1,24 @@
+# Copyright 2009 Marcus Huewe <suse-tux@gmx.de>
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License version 2
+# as published by the Free Software Foundation;
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+
 import mmap
 import os
 import stat
 import struct
+
+# format implementation is based on src/copyin.c and src/util.c (see cpio sources)
 
 class CpioError(Exception):
     """base class for all cpio related errors"""
