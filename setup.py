0d27064a28b453ad2d832d8201b92aa743cd1328
Michal Cihar <mcihar@suse.cz>
2009-05-06 09:33:18 +0000

    Add some metainformation to setup.py and also include correct version there (it is later used in generated egg file).

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+import osc.core
 
 setup(name='osc',
-      version='0.7',
-      description='opensuse commander',
+      version=osc.core.__version__,
+      description='openSUSE (buildsystem) commander',
+      long_description='Commandline client for the openSUSE Build Service, which allows to access repositories in the openSUSE Build Service in similar way as Subversion repositories.',
       author='openSUSE project',
       author_email='opensuse-buildservice@opensuse.org',
       license='GPL',
+      platforms = ['Linux'],
+      keywords = ['openSUSE', 'SUSE', 'RPM', 'build', 'buildservice'],
       url='https://forgesvn1.novell.com/svn/opensuse/trunk/buildservice/src/clientlib/python/osc/',
 
       packages=['osc', 'osc.util'],
