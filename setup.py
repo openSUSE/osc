1bb63e4ab7130739883b4b8efcc82f46d9785e38
Marcus Hüwe <suse-tux@gmx.de>
2009-05-09 14:16:18 +0000

    - fixed setup.py - compress manpage

--- setup.py
+++ setup.py
@@ -16,15 +16,17 @@ class build_osc(distutils.command.build.build, object):
     def build_man_page(self):
         """
         """
-        man_path = os.path.join('build', 'osc.1')
+        import gzip
+        man_path = os.path.join('build', 'osc.1.gz')
         distutils.log.info('generating %s' % man_path)
-        outfile = file(man_path, 'w')
+        outfile = gzip.open(man_path, 'w')
         osccli = commandline.Osc(stdout = outfile)
         osccli.main(argv = ['osc','man'])
+        outfile.close()
 
     def run(self):
-        self.build_man_page()
         super(build_osc, self).run()
+        self.build_man_page()
 
 setup(name='osc',
       version=osc.core.__version__,
@@ -39,7 +41,7 @@ setup(name='osc',
 
       packages=['osc', 'osc.util'],
       scripts=['osc_hotshot.py', 'osc-wrapper.py'],
-      data_files=[(os.path.join('share','man','man1'), [os.path.join('build', 'osc.1')])],
+      data_files=[(os.path.join('share','man','man1'), [os.path.join('build', 'osc.1.gz')])],
 
       # Override certain command classes with our own ones
       cmdclass = {
