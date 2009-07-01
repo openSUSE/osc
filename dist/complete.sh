8f10292166e20703c8337ee528584feb692dbabb
Dr. Werner Fink <werner@suse.de>
2009-07-01 13:04:19 +0000

    Fix bug bnc#518173 and make osc.complete fit new syntax

--- dist/complete.sh
+++ dist/complete.sh
@@ -1,4 +1,6 @@
 test -z "$BASH_VERSION" && return
 complete -o default _nullcommand &> /dev/null || return
 complete -r _nullcommand &> /dev/null         || return
-complete -o default -C /usr/lib/osc/complete osc
+test -s /usr/share/osc/complete && complete -o default -C /usr/share/osc/complete osc
+test -s /usr/lib64/osc/complete && complete -o default -C /usr/lib64/osc/complete osc
+test -s /usr/lib/osc/complete   && complete -o default -C /usr/lib/osc/complete osc
