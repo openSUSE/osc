b7d7508b9622e6eeb9abcc3ea6a2ce44e5dcd9d2
Adrian Schröter <adrian@suse.de>
2010-01-20 10:04:49 +0100

    create 0.125.2

--- osc/core.py
+++ osc/core.py
@@ -5,7 +5,7 @@
 # and distributed under the terms of the GNU General Public Licence,
 # either version 2, or version 3 (at your option).
 
-__version__ = '0.125.1'
+__version__ = '0.125.2'
 
 # __store_version__ is to be incremented when the format of the working copy
 # "store" changes in an incompatible way. Please add any needed migration
