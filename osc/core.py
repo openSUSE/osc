8db2ef82ba9108210b411c9f28021dd3b7895d47
Adrian Schröter <adrian@suse.de>
2010-02-08 11:51:16 +0100

    roll 0.125.5 release ... (this time the maintenance update ?)

--- osc/core.py
+++ osc/core.py
@@ -5,7 +5,7 @@
 # and distributed under the terms of the GNU General Public Licence,
 # either version 2, or version 3 (at your option).
 
-__version__ = '0.125.3'
+__version__ = '0.125.5'
 
 # __store_version__ is to be incremented when the format of the working copy
 # "store" changes in an incompatible way. Please add any needed migration
