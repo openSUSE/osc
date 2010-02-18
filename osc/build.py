openSUSE/osc

commit 8670d82fad0085d39ef7755ef4bda7556e4f517a

- fixed indention - added 'icecream' to  config dict

osc/build.py
@@ -335,19 +335,18 @@ def main(opts, argv):
         build_root = opts.root
     if opts.jobs:
         buildargs.append('--jobs %s' % opts.jobs)
-    else:
-        if config['build-jobs'] > 1:
-            buildargs.append('--jobs %s' % config['build-jobs'])
-    if opts.icecream or 'icecream' in config:
+    elif config['build-jobs'] > 1:
+        buildargs.append('--jobs %s' % config['build-jobs'])
+    if opts.icecream or config['icecream']:
         if opts.icecream:
             num = opts.icecream
         else:
             num = config['icecream']
 
-	if num > 0:
-	    buildargs.append('--icecream %s' % num)
-	    xp.append('icecream')
-	    xp.append('gcc-c++')
+        if num > 0:
+            buildargs.append('--icecream %s' % num)
+            xp.append('icecream')
+            xp.append('gcc-c++')
     if opts.ccache:
         buildargs.append('--ccache')
         xp.append('ccache')

osc/conf.py
@@ -75,6 +75,7 @@ DEFAULTS = { 'apiurl': 'https://api.opensuse.org',
              'build-memory': '',# required for VM builds
              'build-swap': '',  # optional for VM builds
              'build-jobs': os.sysconf('SC_NPROCESSORS_ONLN'), # compile with N jobs
+             'icecream': '0',
 
              'debug': '0',
              'http_debug': '0',

commit f59372c96ea72190ad8ebd1985611e9f15037a30

- fixed check (caused by 8670d82fad0085d39ef7755ef4bda7556e4f517a)

osc/build.py
@@ -337,7 +337,7 @@ def main(opts, argv):
         buildargs.append('--jobs %s' % opts.jobs)
     elif config['build-jobs'] > 1:
         buildargs.append('--jobs %s' % config['build-jobs'])
-    if opts.icecream or config['icecream']:
+    if opts.icecream or config['icecream'] != '0':
         if opts.icecream:
             num = opts.icecream
         else: