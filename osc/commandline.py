openSUSE/osc

commit 54513a23defe7d9052c2ef31ce38fcd70b1488bc

/search/package returns all meta data for the packages, /search/package_id only returns the names - and that's all that interests us for requests. before: 11s after: 5s

osc/core.py
@@ -2632,9 +2632,9 @@ def get_user_projpkgs_request_list(apiurl, user, req_state=('new',), req_type=No
     """Return all new requests for all projects/packages where is user is involved"""
     if not projpkgs:
         res = get_user_projpkgs(apiurl, user, exclude_projects=exclude_projects)
-        for i in res['project'].findall('project'):
+        for i in res['project_id'].findall('project'):
             projpkgs[i.get('name')] = []
-        for i in res['package'].findall('package'):
+        for i in res['package_id'].findall('package'):
             if not i.get('project') in projpkgs.keys():
                 projpkgs.setdefault(i.get('project'), []).append(i.get('name'))
     xpath = ''
@@ -4683,9 +4683,9 @@ def get_user_projpkgs(apiurl, user, role=None, exclude_projects=[], proj=True, p
     xpath_prj = xpath_join(xpath, excl_prj, op='and')
     what = {}
     if pkg:
-        what['package'] = xpath_pkg
+        what['package_id'] = xpath_pkg
     if proj:
-        what['project'] = xpath_prj
+        what['project_id'] = xpath_prj
     try:
         res = search(apiurl, **what)
     except urllib2.HTTPError, e:

commit b08802e9fe5082adffecca15afbf3d61c87eea98

- fixed do_my (was broken by commit 54513a23defe7d9052c2ef31ce38fcd70b1488bc but nobody noticed it so far)

osc/commandline.py
@@ -4838,20 +4838,21 @@ Please submit there instead, or use --nodevelproject to force direct submission.
         if opts.all:
             role_filter = ''
 
-        res = get_user_projpkgs(apiurl, user, role_filter,
-                                exclude_projects, what.has_key('project'), what.has_key('package'), opts.maintained)
+        res = get_user_projpkgs(apiurl, user, role_filter, exclude_projects,
+                                what.has_key('project'), what.has_key('package'),
+                                opts.maintained, opts.verbose)
         request_todo = {}
         roles = {}
         if len(what.keys()) == 2:
-            for i in res['project_id'].findall('project'):
+            for i in res.get('project_id', res.get('project', {})).findall('project'):
                 request_todo[i.get('name')] = []
                 roles[i.get('name')] = [p.get('role') for p in i.findall('person') if p.get('userid') == user]
-            for i in res['package_id'].findall('package'):
+            for i in res.get('package_id', res.get('package', {})).findall('package'):
                 roles['/'.join([i.get('project'), i.get('name')])] = [p.get('role') for p in i.findall('person') if p.get('userid') == user]
                 if not i.get('project') in request_todo.keys():
                     request_todo.setdefault(i.get('project'), []).append(i.get('name'))
         else:
-            for i in res['project_id'].findall('project'):
+            for i in res.get('project_id', res.get('project', {})).findall('project'):
                 roles[i.get('name')] = [p.get('role') for p in i.findall('person') if p.get('userid') == user]
 
         if list_requests:

osc/core.py
@@ -5560,7 +5560,7 @@ def request_interactive_review(apiurl, request):
         if tmpfile is not None:
             tmpfile.close()
 
-def get_user_projpkgs(apiurl, user, role=None, exclude_projects=[], proj=True, pkg=True, maintained=False):
+def get_user_projpkgs(apiurl, user, role=None, exclude_projects=[], proj=True, pkg=True, maintained=False, metadata=False):
     """Return all project/packages where user is involved."""
     xpath = 'person/@userid = \'%s\'' % user
     excl_prj = ''
@@ -5579,9 +5579,15 @@ def get_user_projpkgs(apiurl, user, role=None, exclude_projects=[], proj=True, p
 
     what = {}
     if pkg:
-        what['package_id'] = xpath_pkg
+        if metadata:
+            what['package'] = xpath_pkg
+        else:
+            what['package_id'] = xpath_pkg
     if proj:
-        what['project_id'] = xpath_prj
+        if metadata:
+            what['project'] = xpath_prj
+        else:
+            what['project_id'] = xpath_prj
     try:
         res = search(apiurl, **what)
     except urllib2.HTTPError, e: