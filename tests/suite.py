cda7ffc0407d62a85171e38ed242f8c3b7172b6e
Marcus Huewe <suse-tux@gmx.de>
2010-10-09 16:25:44 +0200

    - testcases for "init_project"

--- tests/suite.py
+++ tests/suite.py
@@ -6,6 +6,7 @@ import test_deletefiles
 import test_revertfiles
 import test_difffiles
 import test_init_package
+import test_init_project
 import test_commit
 import test_repairwc
 
@@ -16,6 +17,7 @@ suite.addTests(test_revertfiles.suite())
 suite.addTests(test_update.suite())
 suite.addTests(test_difffiles.suite())
 suite.addTests(test_init_package.suite())
+suite.addTests(test_init_project.suite())
 suite.addTests(test_commit.suite())
 suite.addTests(test_repairwc.suite())
 result = unittest.TextTestRunner(verbosity=1).run(suite)
---
+++ tests/test_init_project.py
@@ -0,0 +1,62 @@
+import osc.core
+import osc.oscerr
+import os
+import sys
+from common import GET, OscTestCase
+FIXTURES_DIR = os.path.join(os.getcwd(), 'init_project_fixtures')
+
+def suite():
+    import unittest
+    return unittest.makeSuite(TestInitProject)
+
+class TestInitProject(OscTestCase):
+    def _get_fixtures_dir(self):
+        # workaround for git because it doesn't allow empty dirs
+        if not os.path.exists(os.path.join(FIXTURES_DIR, 'osctest')):
+            os.mkdir(os.path.join(FIXTURES_DIR, 'osctest'))
+        return FIXTURES_DIR
+
+    def tearDown(self):
+        if os.path.exists(os.path.join(FIXTURES_DIR, 'osctest')):
+            os.rmdir(os.path.join(FIXTURES_DIR, 'osctest'))
+        OscTestCase.tearDown(self)
+
+    def test_simple(self):
+        """initialize a project dir"""
+        prj_dir = os.path.join(self.tmpdir, 'testprj')
+        osc.core.Project.init_project('http://localhost', prj_dir, 'testprj')
+        storedir = os.path.join(prj_dir, osc.core.store)
+        self._check_list(os.path.join(storedir, '_project'), 'testprj\n')
+        self._check_list(os.path.join(storedir, '_apiurl'), 'http://localhost\n')
+        self._check_list(os.path.join(storedir, '_packages'), '<project name="testprj" />')
+
+    def test_dirExists(self):
+        """initialize a project dir but the dir already exists"""
+        prj_dir = os.path.join(self.tmpdir, 'testprj')
+        os.mkdir(prj_dir)
+        osc.core.Project.init_project('http://localhost', prj_dir, 'testprj')
+        storedir = os.path.join(prj_dir, osc.core.store)
+        self._check_list(os.path.join(storedir, '_project'), 'testprj\n')
+        self._check_list(os.path.join(storedir, '_apiurl'), 'http://localhost\n')
+        self._check_list(os.path.join(storedir, '_packages'), '<project name="testprj" />')
+
+    def test_storedirExists(self):
+        """initialize a project dir but the storedir already exists"""
+        prj_dir = os.path.join(self.tmpdir, 'testprj')
+        os.mkdir(prj_dir)
+        os.mkdir(os.path.join(prj_dir, osc.core.store))
+        self.assertRaises(osc.oscerr.OscIOError, osc.core.Project.init_project, 'http://localhost', prj_dir, 'testprj')
+
+    def test_no_package_tracking(self):
+        """initialize a project dir but disable package tracking"""
+        prj_dir = os.path.join(self.tmpdir, 'testprj')
+        os.mkdir(prj_dir)
+        osc.core.Project.init_project('http://localhost', prj_dir, 'testprj', False)
+        storedir = os.path.join(prj_dir, osc.core.store)
+        self._check_list(os.path.join(storedir, '_project'), 'testprj\n')
+        self._check_list(os.path.join(storedir, '_apiurl'), 'http://localhost\n')
+        self.assertFalse(os.path.exists(os.path.join(storedir, '_packages')))
+
+if __name__ == '__main__':
+    import unittest
+    unittest.main()
