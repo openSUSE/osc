21b7f35274330d2a05d9abea043e45f13b2e6efb
Marcus Huewe <suse-tux@gmx.de>
2010-12-07 20:35:12 +0100

    - potential fix for #657838("osc 0.130: osc repairwc fails totally - working copy useless")
    - do_repairwc: added '--force-apiurl' option:
      ask once for an apiurl and force this apiurl for all inconsistent projects/packages

--- osc/commandline.py
+++ osc/commandline.py
@@ -6102,6 +6102,8 @@ Please submit there instead, or use --nodevelproject to force direct submission.
             for f in p.todo:
                 p.revert(f)
 
+    @cmdln.option('--force-apiurl', action='store_true',
+                  help='ask once for an apiurl and force this apiurl for all inconsistent projects/packages')
     def do_repairwc(self, subcmd, opts, *args):
         """${cmd_name}: try to repair an inconsistent working copy
 
@@ -6135,13 +6137,13 @@ Please submit there instead, or use --nodevelproject to force direct submission.
         pacs = []
         cp = conf.get_configParser(conf.config['conffile'])
         apiurls = [i.rstrip('/') for i in cp.sections() if i != 'general']
+        apiurl = ''
         for i in args:
             if is_project_dir(i):
                 try:
                     prj = Project(i, getPackageList=False)
                 except oscerr.WorkingCopyInconsistent, e:
-                    apiurl = None
-                    if '_apiurl' in e.dirty_files:
+                    if '_apiurl' in e.dirty_files and (not apiurl or not opts.force_apiurl):
                         apiurl = get_apiurl(apiurls)
                     prj = Project(i, getPackageList=False, wc_check=False)
                     prj.wc_repair(apiurl)
@@ -6161,8 +6163,7 @@ Please submit there instead, or use --nodevelproject to force direct submission.
             try:
                 p = Package(pdir)
             except oscerr.WorkingCopyInconsistent, e:
-                apiurl = None
-                if '_apiurl' in e.dirty_files:
+                if '_apiurl' in e.dirty_files and (not apiurl or not opts.force_apiurl):
                     apiurl = get_apiurl(apiurls)
                 p = Package(pdir, wc_check=False)
                 p.wc_repair(apiurl)
