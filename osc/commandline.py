4b44e7470b9504d69ecfd25adbb6d880c0bfbb40
Dirk Müller <dmueller@suse.com>
2012-09-10 10:43:28 +0200

    update default list of request states
    Hi,
    
    "declined" was some time ago changed to be considered an "open" request state.
    
    the default behavior of "osc rq list" is IMHO to show open requests (requests
    in state new or review), so I'm attaching a patch that adds "declined" as
    well.
    
    Tia,
    Dirk
    
    From 73fd10a2203be8e9dc215106648b2d9686d8fdc5 Mon Sep 17 00:00:00 2001
    From: Dirk Mueller <dmueller@suse.com>
    Date: Mon, 10 Sep 2012 10:40:48 +0200
    Subject: [PATCH] add 'declined' to list of default states for rq list
    
    osc rq list should show all currently "open" requests. "declined"
    is an open request state, so it should be included by default.

--- osc/commandline.py
+++ osc/commandline.py
@@ -1747,7 +1747,7 @@ Please submit there instead, or use --nodevelproject to force direct submission.
                         help='all states. Same as\'-s all\'')
     @cmdln.option('-f', '--force', action='store_true',
                         help='enforce state change, can be used to ignore open reviews')
-    @cmdln.option('-s', '--state', default='',  # default is 'all' if no args given, 'new,review' otherwise
+    @cmdln.option('-s', '--state', default='',  # default is 'all' if no args given, 'declined,new,review' otherwise
                         help='only list requests in one of the comma separated given states (new/review/accepted/revoked/declined) or "all" [default="new,review", or "all", if no args given]')
     @cmdln.option('-D', '--days', metavar='DAYS',
                         help='only list requests in state "new" or changed in the last DAYS. [default=%(request_list_days)s]')
@@ -1871,7 +1871,7 @@ Please submit there instead, or use --nodevelproject to force direct submission.
                 opts.state = 'all'
 
         if opts.state == '':
-            opts.state = 'new,review'
+            opts.state = 'declined,new,review'
 
         if args[0] == 'help':
             return self.do_help(['help', 'request'])
