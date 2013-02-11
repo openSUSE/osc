b358e7085f21c09afc866b19c59bef5b404925e1
Adrian Schröter <adrian@suse.de>
2013-02-11 13:01:58 +0100

    - remove debug output

--- osc/core.py
+++ osc/core.py
@@ -5301,7 +5301,6 @@ def print_jobhistory(apiurl, prj, current_package, repository, arch, format = 't
         st = int(node.get('starttime'))
         et = int(node.get('endtime'))
         endtime = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(et))
-        print "XXX", et, st
         waittm = time.gmtime(et-st)
         if waittm.tm_mday>1:
             waitbuild = "%1dd %2dh %2dm %2ds" % (waittm.tm_mday-1, waittm.tm_hour, waittm.tm_min, waittm.tm_sec)
