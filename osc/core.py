74f950d4094d7b5b5e55902735620e914ad7630d
Adrian Schröter <adrian@suse.de>
2013-02-25 15:40:26 +0100

    - prepare 0.139 release

--- osc/core.py
+++ osc/core.py
@@ -3,7 +3,7 @@
 # and distributed under the terms of the GNU General Public Licence,
 # either version 2, or version 3 (at your option).
 
-__version__ = '0.138git'
+__version__ = '0.139'
 
 # __store_version__ is to be incremented when the format of the working copy
 # "store" changes in an incompatible way. Please add any needed migration
