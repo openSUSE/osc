744ce9155f261a61b6e146324df0a2effbc32937
Adrian Schröter <adrian@suse.de>
2013-09-17 13:49:53 +0200

    - release 0.141

--- osc/core.py
+++ osc/core.py
@@ -5,7 +5,7 @@
 
 from __future__ import print_function
 
-__version__ = '0.140git'
+__version__ = '0.141'
 
 # __store_version__ is to be incremented when the format of the working copy
 # "store" changes in an incompatible way. Please add any needed migration
