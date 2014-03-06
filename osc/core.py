openSUSE/osc

commit 6469efb9eded4d378fa9282dd9cc447f11bf2b97

- core.get_local_origin_project: added check for None

osc/core.py
@@ -1560,7 +1560,13 @@ class Package:
 
     def get_local_origin_project(self):
         """Get the originproject from the _meta file."""
-        root = ET.fromstring(self.get_local_meta())
+        # if the wc was checked out via some old osc version
+        # there might be no meta file: in this case we assume
+        # that the origin project is equal to the wc's project
+        meta = self.get_local_meta()
+        if meta is None:
+            return self.project
+        root = ET.fromstring(meta)
         return root.get('project')
 
     def is_link_to_different_project(self):

commit 720c685868e8f76ccdf86685a0239267faa0bfbd

- core.Package.commit: make sure a _meta is created for newly added packages
Fixed typo (introduced in commit 6469efb9eded4d378fa9282dd9cc447f11bf2b97).

osc/core.py
@@ -1453,6 +1453,10 @@ class Package:
             print('')
             rev = self.latest_rev()
             self.update(rev=rev)
+        elif self.get_local_meta() is None:
+            # if this was a newly added package there is no _meta
+            # file
+            self.update_local_pacmeta()
 
     def __write_storelist(self, name, data):
         if len(data) == 0:
@@ -1565,7 +1569,7 @@ class Package:
         # that the origin project is equal to the wc's project
         meta = self.get_local_meta()
         if meta is None:
-            return self.project
+            return self.prjname
         root = ET.fromstring(meta)
         return root.get('project')
 