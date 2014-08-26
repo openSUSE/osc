3606506d1eb2f1630f8bb246b1909fb7fe6ef909
Adrian Schröter <adrian@suse.de>
2014-08-26 09:49:56 +0200

    - prepare 0.147 release

--- osc/core.py
+++ osc/core.py
@@ -5,7 +5,7 @@
 
 from __future__ import print_function
 
-__version__ = '0.145git'
+__version__ = '0.147'
 
 # __store_version__ is to be incremented when the format of the working copy
 # "store" changes in an incompatible way. Please add any needed migration
