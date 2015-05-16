56a9ab60dc77f5dcbf1b29b7846c09f8d80c138c
Marcus Huewe <suse-tux@gmx.de>
2015-05-16 22:49:17 +0200

    changed commit 22b272f3afc1a3f3b83b764b7745a662bc0aa234 a bit
    - only "fix" sys.stdout if it is no tty
    - make sys.stdout line buffered (to avoid too "many" write calls)
    - added more detailed explanation why this is needed

--- osc-wrapper.py
+++ osc-wrapper.py
@@ -23,8 +23,18 @@ except NameError:
     pass
 
 # avoid buffering output on pipes (bnc#930137)
-if sys.stdout.name == '<stdout>':
-    sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
+# Basically, a "print('foo')" call is translated to a corresponding
+# fwrite call that writes to the stdout stream (cf. string_print
+# (Objects/stringobject.c) and builtin_print (Python/bltinmodule.c));
+# If no pipe is used, stdout is a tty/refers to a terminal =>
+# the stream is line buffered (see _IO_file_doallocate (libio/filedoalloc.c)).
+# If a pipe is used, stdout does not refer to a terminal anymore =>
+# the stream is fully buffered by default (see _IO_file_doallocate).
+# The following fdopen call makes stdout line buffered again (at least on
+# systems that support setvbuf - if setvbuf is not supported, the stream
+# remains fully buffered (see PyFile_SetBufSize (Objects/fileobject.c))).
+if not os.isatty(sys.stdout.fileno()):
+    sys.stdout = os.fdopen(sys.stdout.fileno(), sys.stdout.mode, 1)
 
 osccli = commandline.Osc()
 
