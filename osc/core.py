7f428cccdfaf84483478e1d0ccacbb670409686f
Marco Strigl <mstrigl@suse.com>
2017-03-08 09:25:02 +0100

    release 0.157.0

--- osc/core.py
+++ osc/core.py
@@ -5,7 +5,7 @@
 
 from __future__ import print_function
 
-__version__ = '0.156git'
+__version__ = '0.157'
 
 # __store_version__ is to be incremented when the format of the working copy
 # "store" changes in an incompatible way. Please add any needed migration
