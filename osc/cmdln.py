ca8fe4273a6d4b1f01f2cd05eaf19a40d1082495
Andrew Shadura <andrew.shadura@collabora.co.uk>
2017-09-02 13:30:37 +0200

    Allow whitespace before ${cmd_name}.

--- osc/cmdln.py
+++ osc/cmdln.py
@@ -788,10 +788,10 @@ class RawCmdln(cmd.Cmd):
             # practice dictates that command help strings begin with this, but
             # it isn't at all wanted for the command list.
             to_strip = "${cmd_name}:"
-            if doc and doc.startswith(to_strip):
+            if doc and doc.lstrip().startswith(to_strip):
                 #log.debug("stripping %r from start of %s's help string",
                 #          to_strip, cmdname)
-                doc = doc[len(to_strip):].lstrip()
+                doc = (doc.lstrip())[len(to_strip):].lstrip()
             if not getattr(self._get_cmd_handler(cmdname), "hidden", None):
                 linedata.append( (cmdstr, doc) )
 
