0eecdaf8301760ed4dbfe2d2eb7b608046bee7fc
Marcus Huewe <suse-tux@gmx.de>
2017-10-10 16:35:47 +0200

    Add missing comment
    Follow-up commit for f6f879d.

--- osc/core.py
+++ osc/core.py
@@ -6677,6 +6677,8 @@ def unpack_srcrpm(srpm, dir, *files):
     with open(srpm, 'r') as fsrpm, open(os.devnull, 'w') as devnull:
         rpm2cpio_proc = subprocess.Popen(['rpm2cpio'], stdin=fsrpm,
                                          stdout=subprocess.PIPE)
+        # XXX: shell injection is possible via the files parameter, but the
+        #      current osc code does not use the files parameter.
         cpio_proc = subprocess.Popen(['cpio', '-i'] + list(files),
                                      stdin=rpm2cpio_proc.stdout, stderr=devnull)
         rpm2cpio_proc.stdout.close()
