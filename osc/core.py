72f9f89e859398a2701c22e42732a81bec6e6cea
marco <lethliel@gmx.de>
2017-10-26 13:33:06 +0200

    prepare 0.161 release

--- osc/core.py
+++ osc/core.py
@@ -5,7 +5,7 @@
 
 from __future__ import print_function
 
-__version__ = '0.160git'
+__version__ = '0.161'
 
 # __store_version__ is to be incremented when the format of the working copy
 # "store" changes in an incompatible way. Please add any needed migration
