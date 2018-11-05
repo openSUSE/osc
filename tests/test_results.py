5e03ffcec96f3c86ae2c8ff75bc346f0ca9c8874
Jan Zerebecki <jzerebecki@suse.com>
2018-11-05 22:57:14 +0100

    Add --watch for osc prjresults --xml
    Like: osc results --watch

--- tests/test_results.py
+++ tests/test_results.py
@@ -33,6 +33,12 @@ class TestResults(OscTestCase):
         out = self._run_osc('prjresults', '--xml', 'testproject')
         self.assertEqualMultiline(out, self._get_fixture('result.xml')+'\n')
 
+    @GET('http://localhost/build/testproject/_result', file='result-dirty.xml')
+    @GET('http://localhost/build/testproject/_result?oldstate=c57e2ee592dbbf26ebf19cc4f1bc1e83', file='result.xml')
+    def testPrjresultsWatch(self):
+        out = self._run_osc('prjresults', '--watch', '--xml', 'testproject')
+        self.assertEqualMultiline(out, self._get_fixture('result-dirty.xml')+'\n'+self._get_fixture('result.xml')+'\n')
+
     @GET('http://localhost/build/testproject/_result?package=python-MarkupSafe&multibuild=1&locallink=1', file='result.xml')
     def testResults(self):
         out = self._run_osc('results', '--xml', 'testproject', 'python-MarkupSafe')
@@ -43,4 +49,8 @@ class TestResults(OscTestCase):
     def testResultsWatch(self):
         out = self._run_osc('results', '--watch', '--xml', 'testproject', 'python-MarkupSafe')
         self.assertEqualMultiline(out, self._get_fixture('result-dirty.xml')+self._get_fixture('result.xml'))
+
+if __name__ == '__main__':
+    import unittest
+    unittest.main()
 
