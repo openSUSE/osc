fefd7c1dea3cdedbfb64f335f61f7e8badaee1a3
Marcus Huewe <suse-tux@gmx.de>
2019-01-29 14:20:12 +0100

    Move vc env exporting code into core.vc_export_env
    This allows for reuse. In the future, these variables should also
    be exported when executing source services.

--- osc/commandline.py
+++ osc/commandline.py
@@ -8850,37 +8850,6 @@ Please submit there instead, or use --nodevelproject to force direct submission.
         else:
             apiurl = self.get_api_url()
 
-        # try to set the env variables for the user's realname and email
-        # (the variables are used by the "vc" script)
-        tag2envs = {'realname': ['VC_REALNAME'],
-                    'email': ['VC_MAILADDR', 'mailaddr']}
-        tag2val = {}
-        missing_tags = []
-
-        for (tag, envs) in tag2envs.items():
-            env_present = [env for env in envs if env in os.environ]
-            config_present = tag in conf.config['api_host_options'][apiurl]
-            if not env_present and not config_present:
-                missing_tags.append(tag)
-            elif config_present:
-                tag2val[tag] = conf.config['api_host_options'][apiurl][tag]
-
-        if missing_tags:
-            user = conf.get_apiurl_usr(apiurl)
-            data = get_user_data(apiurl, user, *missing_tags)
-            if data is not None:
-                for tag in missing_tags:
-                    val = data.pop(0)
-                    if val != '-':
-                        tag2val[tag] = val
-                    else:
-                        msg = 'Try env %s=...' % tag2envs[tag][0]
-                        print(msg, file=sys.stderr)
-
-        for (tag, val) in tag2val.items():
-            for env in tag2envs[tag]:
-                os.environ[env] = val
-
         if meego_style:
             if opts.message or opts.just_edit:
                 print('Warning: to edit MeeGo style changelog, opts will be ignored.', file=sys.stderr)
@@ -8899,6 +8868,7 @@ Please submit there instead, or use --nodevelproject to force direct submission.
 
             cmd_list.extend(args)
 
+        vc_export_env(apiurl)
         vc = Popen(cmd_list)
         vc.wait()
         sys.exit(vc.returncode)
