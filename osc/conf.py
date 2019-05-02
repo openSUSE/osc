396a11268fe64b079578fca77a40f4b9f3e5f35f
Stephan Kulow <coolo@suse.de>
2019-05-02 08:21:22 +0200

    Don't warn about disabled SSL for http URLs
    During development and testing against a local OBS, there is no point
    in warning about disabled SSL checks, osc code itself disables sslchk
    for http URLs.

--- osc/conf.py
+++ osc/conf.py
@@ -562,7 +562,9 @@ def _build_opener(apiurl):
             handlers.append(HTTPSHandler(context=ctx))
         except AttributeError:
             pass
-        print("WARNING: SSL certificate checks disabled. Connection is insecure!\n", file=sys.stderr)
+        scheme, host, path = parse_apisrv_url(config.get('scheme', 'https'), apiurl)
+        if scheme != 'http':
+            print("WARNING: SSL certificate checks disabled. Connection is insecure!\n", file=sys.stderr)
         opener = build_opener(*handlers)
     opener.addheaders = [('User-agent', 'osc/%s' % __version__)]
     _build_opener.last_opener = (apiurl, opener)
