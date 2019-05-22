1ab68d3bfbca9dfbe290b22df6535856790bc393
lethliel <mstrigl@suse.com>
2019-05-22 13:00:55 +0200

    open 0.165.2 development

--- osc/core.py
+++ osc/core.py
@@ -5,7 +5,7 @@
 
 from __future__ import print_function
 
-__version__ = '0.165.1'
+__version__ = '0.165.2.git'
 
 # __store_version__ is to be incremented when the format of the working copy
 # "store" changes in an incompatible way. Please add any needed migration
