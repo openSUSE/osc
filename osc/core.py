2a2ea5c27d17568432912b890e06ef2527f1b100
lethliel <mstrigl@suse.de>
2019-12-05 10:44:46 +0100

    release 0.167.0

--- osc/core.py
+++ osc/core.py
@@ -5,7 +5,7 @@
 
 from __future__ import print_function
 
-__version__ = '0.166.2'
+__version__ = '0.167.0'
 
 # __store_version__ is to be incremented when the format of the working copy
 # "store" changes in an incompatible way. Please add any needed migration
