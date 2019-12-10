openSUSE/osc

commit db795c81219fad406b7fd19341ed29530cb45c9b

support local building using RedHat rpm-md modules

osc/fetch.py
@@ -28,7 +28,8 @@ from .meter import create_text_meter
 
 class Fetcher:
     def __init__(self, cachedir='/tmp', api_host_options={}, urllist=[],
-            http_debug=False, cookiejar=None, offline=False, enable_cpio=True):
+                 modules = [], http_debug=False, cookiejar=None, offline=False,
+                 enable_cpio=True):
         # set up progress bar callback
         self.progress_obj = None
         if sys.stdout.isatty():
@@ -36,6 +37,7 @@ class Fetcher:
 
         self.cachedir = cachedir
         self.urllist = urllist
+        self.modules = modules
         self.http_debug = http_debug
         self.offline = offline
         self.cpio = {}
@@ -59,6 +61,8 @@ class Fetcher:
             return
         query = ['binary=%s' % quote_plus(i) for i in pkgs]
         query.append('view=cpio')
+        for module in self.modules:
+            query.append('module=' + module)
         try:
             url = makeurl(apiurl, ['build', project, repo, arch, package], query=query)
             sys.stdout.write("preparing download ...\r")

osc/build.py
@@ -145,6 +145,9 @@ class Buildinfo:
         self.keys = []
         self.prjkeys = []
         self.pathes = []
+        self.modules = []
+        for node in root.findall('module'):
+            self.modules.append(node.text)
         for node in root.findall('bdep'):
             p = Pac(node, self.buildarch, self.pacsuffix,
                     apiurl, localpkgs)
@@ -939,6 +942,7 @@ def main(apiurl, opts, argv):
                       api_host_options = config['api_host_options'],
                       offline = opts.noinit or opts.offline,
                       http_debug = config['http_debug'],
+                      modules = bi.modules,
                       enable_cpio = not opts.disable_cpio_bulk_download,
                       cookiejar=cookiejar)
 

commit ef4e0411e9d3aa31f3cf3133790184e01461d155

fix Fetch api to become compatible again.
Broke in db795c81219fad406b7fd19341ed29530cb45c9b as reported by Marcus

osc/fetch.py
@@ -28,8 +28,8 @@ from .meter import create_text_meter
 
 class Fetcher:
     def __init__(self, cachedir='/tmp', api_host_options={}, urllist=[],
-                 modules = [], http_debug=False, cookiejar=None, offline=False,
-                 enable_cpio=True):
+                 http_debug=False, cookiejar=None, offline=False,
+                 enable_cpio=True, modules=[]):
         # set up progress bar callback
         self.progress_obj = None
         if sys.stdout.isatty():