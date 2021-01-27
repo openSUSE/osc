5df5465b1ae21004e6f3974e357fb55a2df62188
Marcus Huewe <suse-tux@gmx.de>
2021-01-27 13:21:32 +0100

    Release 0.172.0
    For the details see the NEWS file.

--- osc/core.py
+++ osc/core.py
@@ -5,7 +5,7 @@
 
 from __future__ import print_function
 
-__version__ = '0.172.git'
+__version__ = '0.172.0'
 
 # __store_version__ is to be incremented when the format of the working copy
 # "store" changes in an incompatible way. Please add any needed migration
