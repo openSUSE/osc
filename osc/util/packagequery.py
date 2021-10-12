b5d337d0376ef78e11e3d0128bb5dc17fa468c8f
Adrian Schröter <adrian@suse.de>
2021-10-12 10:54:20 +0200

    fix hdmrd5 check of local cached files
    Current OBS is delivering hdrmd5 in buildinfo. It turns out
    that osc has already code for validating cached files, but it
    invalidates all local files atm with python 3.x

--- osc/util/packagequery.py
+++ osc/util/packagequery.py
@@ -87,7 +87,7 @@ class PackageQuery:
         f = open(filename, 'rb')
         magic = f.read(7)
         f.seek(0)
-        if magic[:4] == '\xed\xab\xee\xdb':
+        if magic[:4] == b'\xed\xab\xee\xdb':
             from . import rpmquery
             f.close()
             return rpmquery.RpmQuery.queryhdrmd5(filename)
