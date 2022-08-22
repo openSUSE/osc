2892f1bb1121e70b00e0102e391d79c4daa7cf30
Daniel Mach <daniel.mach@suse.com>
2022-08-22 11:34:56 +0200

    Do not list declined requests by default

--- osc/commandline.py
+++ osc/commandline.py
@@ -2140,8 +2140,8 @@ Please submit there instead, or use --nodevelproject to force direct submission.
                         help='all states. Same as\'-s all\'')
     @cmdln.option('-f', '--force', action='store_true',
                         help='enforce state change, can be used to ignore open reviews')
-    @cmdln.option('-s', '--state', default='',  # default is 'all' if no args given, 'declined,new,review' otherwise
-                        help='only list requests in one of the comma separated given states (new/review/accepted/revoked/declined) or "all" [default="declined,new,review", or "all", if no args given]')
+    @cmdln.option('-s', '--state', default='',  # default is 'all' if no args given, 'new,review' otherwise
+                        help='only list requests in one of the comma separated given states (new/review/accepted/revoked/declined) or "all" [default="new,review", or "all", if no args given]')
     @cmdln.option('-D', '--days', metavar='DAYS',
                         help='only list requests in state "new" or changed in the last DAYS.')
     @cmdln.option('-U', '--user', metavar='USER',
@@ -2278,7 +2278,7 @@ Please submit there instead, or use --nodevelproject to force direct submission.
                 opts.state = 'all'
 
         if opts.state == '' and subcmd != 'review':
-            opts.state = 'declined,new,review'
+            opts.state = 'new,review'
 
         if opts.incoming:
             conf.config['include_request_from_project'] = False
