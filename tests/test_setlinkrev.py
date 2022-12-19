0384db5e2e1b29b82d26f48ab7cfaa422ad09cc6
Daniel Mach <daniel.mach@suse.com>
2022-12-19 14:04:01 +0100

    setlinkrev: Write a log message on setting a revision

--- tests/test_setlinkrev.py
+++ tests/test_setlinkrev.py
@@ -23,14 +23,14 @@ class TestSetLinkRev(OscTestCase):
 
     @GET('http://localhost/source/osctest/simple/_link', file='simple_link')
     @GET('http://localhost/source/srcprj/srcpkg?rev=latest', file='simple_filesremote')
-    @PUT('http://localhost/source/osctest/simple/_link',
+    @PUT('http://localhost/source/osctest/simple/_link?comment=Set+link+revision+to+42',
          exp='<link package="srcpkg" project="srcprj" rev="42" />', text='dummytext')
     def test_simple1(self):
         """a simple set_link_rev call without revision"""
         osc.core.set_link_rev('http://localhost', 'osctest', 'simple')
 
     @GET('http://localhost/source/osctest/simple/_link', file='simple_link')
-    @PUT('http://localhost/source/osctest/simple/_link',
+    @PUT('http://localhost/source/osctest/simple/_link?comment=Set+link+revision+to+42',
          exp='<link package="srcpkg" project="srcprj" rev="42" />', text='dummytext')
     def test_simple2(self):
         """a simple set_link_rev call with revision"""
@@ -38,7 +38,7 @@ class TestSetLinkRev(OscTestCase):
 
     @GET('http://localhost/source/osctest/simple/_link', file='noproject_link')
     @GET('http://localhost/source/osctest/srcpkg?rev=latest&expand=1', file='expandedsrc_filesremote')
-    @PUT('http://localhost/source/osctest/simple/_link',
+    @PUT('http://localhost/source/osctest/simple/_link?comment=Set+link+revision+to+eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee',
          exp='<link package="srcpkg" rev="eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" vrev="1" />', text='dummytext')
     def test_expandedsrc(self):
         """expand src package"""
@@ -46,7 +46,7 @@ class TestSetLinkRev(OscTestCase):
 
     @GET('http://localhost/source/osctest/simple/_link', file='link_with_rev')
     @GET('http://localhost/source/srcprj/srcpkg?rev=latest', file='simple_filesremote')
-    @PUT('http://localhost/source/osctest/simple/_link',
+    @PUT('http://localhost/source/osctest/simple/_link?comment=Set+link+revision+to+42',
          exp='<link package="srcpkg" project="srcprj" rev="42" />', text='dummytext')
     def test_existingrev(self):
         """link already has a rev attribute, update it to current version"""
@@ -55,7 +55,7 @@ class TestSetLinkRev(OscTestCase):
 
     @GET('http://localhost/source/osctest/simple/_link', file='link_with_rev')
     @GET('http://localhost/source/srcprj/srcpkg?rev=latest&expand=1', file='expandedsrc_filesremote')
-    @PUT('http://localhost/source/osctest/simple/_link',
+    @PUT('http://localhost/source/osctest/simple/_link?comment=Set+link+revision+to+eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee',
          exp='<link package="srcpkg" project="srcprj" rev="eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" vrev="1" />',
          text='dummytext')
     def test_expandexistingrev(self):
@@ -71,21 +71,21 @@ class TestSetLinkRev(OscTestCase):
         self.assertRaises(HTTPError, osc.core.set_link_rev, 'http://localhost', 'osctest', 'simple', expand=True)
 
     @GET('http://localhost/source/osctest/simple/_link', file='rev_link')
-    @PUT('http://localhost/source/osctest/simple/_link',
+    @PUT('http://localhost/source/osctest/simple/_link?comment=Unset+link+revision',
          exp='<link package="srcpkg" project="srcprj" />', text='dummytext')
     def test_deleterev(self):
         """delete rev attribute from link xml"""
         osc.core.set_link_rev('http://localhost', 'osctest', 'simple', revision=None)
 
     @GET('http://localhost/source/osctest/simple/_link', file='md5_rev_link')
-    @PUT('http://localhost/source/osctest/simple/_link',
+    @PUT('http://localhost/source/osctest/simple/_link?comment=Unset+link+revision',
          exp='<link package="srcpkg" project="srcprj" />', text='dummytext')
-    def test_deleterev(self):
+    def test_deleterev_md5(self):
         """delete rev and vrev attribute from link xml"""
         osc.core.set_link_rev('http://localhost', 'osctest', 'simple', revision=None)
 
     @GET('http://localhost/source/osctest/simple/_link', file='simple_link')
-    @PUT('http://localhost/source/osctest/simple/_link',
+    @PUT('http://localhost/source/osctest/simple/_link?comment=Unset+link+revision',
          exp='<link package="srcpkg" project="srcprj" />', text='dummytext')
     def test_deleterevnonexistent(self):
         """delete non existent rev attribute from link xml"""
