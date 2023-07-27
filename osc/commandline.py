7943b55a6ed144ff75c60fd0b88c20decea0a61e
Daniel Mach <daniel.mach@suse.com>
2023-07-27 13:29:38 +0200

    Add limited support of Git SCM to the 'build' command

--- osc/commandline.py
+++ osc/commandline.py
@@ -30,6 +30,7 @@ from . import build as osc_build
 from . import cmdln
 from . import commands as osc_commands
 from . import conf
+from . import git_scm
 from . import oscerr
 from . import store as osc_store
 from .core import *
@@ -7197,11 +7198,16 @@ Please submit there instead, or use --nodevelproject to force direct submission.
         if len(args) > 3:
             raise oscerr.WrongArgs('Too many arguments')
 
-        store = osc_store.Store(Path.cwd())
+        store = osc_store.get_store(Path.cwd(), print_warnings=True)
         store.assert_is_package()
 
         if opts.alternative_project == store.project:
             opts.alternative_project = None
+
+        # HACK: avoid calling some underlying store_*() functions from parse_repoarchdescr() method
+        # We'll fix parse_repoarchdescr() later because it requires a larger change
+        if not opts.alternative_project and isinstance(store, git_scm.GitStore):
+            opts.alternative_project = store.project
 
         if len(args) == 0 and store.is_package and store.last_buildroot:
             # build env not specified, just read from last build attempt
