310479b946db6d394ad28373a8212e69dd91719b
Matěj Cepl <mcepl@cepl.eu>
2023-08-10 08:45:55 +0200

    osc prompt for Bash.
    Equivalent of `__git_ps1` for git.
    
    Suggested use:
    
        # Prompt for osc
        [[ -f /usr/share/bash-completion/completions/osc-prompt.sh ]] && \
            source /usr/share/bash-completion/completions/osc-prompt.sh
    
        export PS1='\h:\W$(__osc_prompt) \$ '
    
    Fixes: https://github.com/openSUSE/osc/issues/1379

---
+++ contrib/osc-prompt.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+__osc_prompt() {
+    # Git has a precedence
+    if [ -d .git ] ; then
+        # Test for the existence of bash function
+        declare -F __git_ps1 >/dev/null && printf "%s" "$(__git_ps1 "$@")"
+        return
+    fi
+    # Are we even in the OSC checkout?
+    [ -d .osc ] || return
+
+    local osc_binary osc_pattern osc_str;
+    osc_binary=$(type -p osc)
+    if [ -n "$1" ] ; then osc_pattern="${*}" ; else osc_pattern="(%s)" ; fi
+    if [ -n "$osc_binary" ] && [ -x "$osc_binary" ] && [ -f .osc/_package ] ; then
+        osc_str="$(osc status 2>/dev/null |cut -d' ' -f 1|sort|uniq -c|tr -d ' \n')"
+        # shellcheck disable=SC2059
+        printf " ${osc_pattern}" "$osc_str"
+    fi
+}
