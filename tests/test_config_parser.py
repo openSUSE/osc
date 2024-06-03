360a94c4a3745dfe59863c6a112acf8bcf38840d
Daniel Mach <daniel.mach@suse.com>
2024-06-03 08:21:17 +0200

    Fix config parser to throw an exception on duplicate sections or options

--- tests/test_config_parser.py
+++ tests/test_config_parser.py
@@ -1,3 +1,4 @@
+import configparser
 import unittest
 
 from osc.OscConfigParser import OscConfigParser
@@ -20,6 +21,28 @@ pass=
         # with interpolation on, this would raise
         # ValueError: invalid interpolation syntax in '%' at position 0
         self.parser.set("http://localhost", "pass", "%")
+
+    def test_duplicate_section(self):
+        conf = """
+[general]
+
+[http://localhost]
+
+[http://localhost]
+"""
+        parser = OscConfigParser()
+        self.assertRaises(configparser.DuplicateSectionError, parser.read_string, conf)
+
+    def test_duplicate_option(self):
+        conf = """
+[general]
+
+[http://localhost]
+user=
+user=
+"""
+        parser = OscConfigParser()
+        self.assertRaises(configparser.DuplicateOptionError, parser.read_string, conf)
 
 
 if __name__ == "__main__":
