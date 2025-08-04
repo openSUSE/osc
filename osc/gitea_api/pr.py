a208031331de0ed9964d3d93b4bfc5d84ecce6a1
Andrii Nikitin <anikitin@suse.de>
2025-08-04 14:12:05 +0200

    Dump referenced PRs to metadata/referenced-pull-requests.json

--- osc/gitea_api/pr.py
+++ osc/gitea_api/pr.py
@@ -87,6 +87,9 @@ class PullRequest(GiteaModel):
         """
         match = re.match(r"^([^/]+)/([^/]+)[#!]([0-9]+)$", pr_id)
         if not match:
+            match = re.match(r"^([^/]+)/([^/]+)/pulls/([0-9]+)$", pr_id)
+
+        if not match:
             raise ValueError(f"Invalid pull request id: {pr_id}")
         return match.group(1), match.group(2), int(match.group(3))
 
@@ -264,6 +267,12 @@ class PullRequest(GiteaModel):
         table.add("Description", self.body)
 
         return str(table)
+
+    def to_light_dict(self, exclude_columns: Optional[list] = None):
+        x = ["allow_maintainer_edit", "body"]
+        if exclude_columns:
+            x += exclude_columns
+        return self.dict(x)
 
     def dict(self, exclude_columns: Optional[list] = None):
         import inspect
